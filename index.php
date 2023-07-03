731045c6e791e6e50ea779553db97c43d1cea660
stoicos <136374793+stoicos@users.noreply.github.com>
2023-07-03 00:36:11 -0300

    add fcm support

--- index.php
+++ index.php
@@ -37,6 +37,7 @@ if(!array_key_exists($seccion, $seccionesValidas)){
     <link rel="manifest" href="manifest.json">
 </head>
 <body>
+    <p id="token"></p>
 <div class="container">
     <header>
         <nav>
@@ -69,5 +70,42 @@ if(!array_key_exists($seccion, $seccionesValidas)){
     </footer>
 </div>
 <script src="index.js"></script>
+<script type="module">
+  // Le digo al servicio de google que me configure las cosas que necesito para trabajar con messagin
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+  import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging.js";
+
+  // Configuración de servicio
+  const firebaseConfig = {
+    apiKey: "AIzaSyBvoPNyKvFbeT45FSjnN3yRcljX2qz-7tQ",
+    authDomain: "mis-brownies.firebaseapp.com",
+    projectId: "mis-brownies",
+    storageBucket: "mis-brownies.appspot.com",
+    messagingSenderId: "902119157883",
+    appId: "1:902119157883:web:052fa649d11575cefff2de"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const messaging = getMessaging(app);
+
+  // Configuración básica
+  function initializeFirebaseMessaging() {
+    Notification.requestPermission()
+        .then(function() {
+            console.log("Notification permission granted");
+            return getToken(messaging, {vapidKey: 'AAAA0gp98Hs:APA91bED92StENK22UbYnhbchU0-jYBmE_9Xk7NNjKYYDiuZqy0ca0tMrApM1NoXqktpyPwUKQHwa0tvAyXsDOfHofBxd3q1Cnrf9Sy4BltOosOJ6QkrDcRiaAsyKFk22D7hAusWUBdD'});
+        })
+        .then(function(token) {
+            console.log("Token:" + token);
+            document.getElementById("token").innerHTML = token;
+        })
+        .catch(function(err) {
+            console.log("Unable to get permission to notify.", err);
+        })
+  };
+
+  initializeFirebaseMessaging();
+</script>
 </body>
 </html>
