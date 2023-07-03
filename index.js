ff75743f74d129b31947a9958141372c1417c787
stoicos <136374793+stoicos@users.noreply.github.com>
2023-07-02 23:55:41 -0300

    add changes

--- index.js
+++ index.js
@@ -42,22 +42,47 @@ window.addEventListener('DOMContentLoaded', function() {
     
     BUTTON_FETCH.addEventListener('click', function(){
 
-        for(let i = 0; i < 3; i++){
-            fetch('https://randomuser.me/api/')
-            .then(response => response.json())
-            .then(data => {
 
-                DIV.innerHTML += `
+        let socios = [];
+        socios.push(JSON.parse(localStorage.getItem(`socio0`)));
+        socios.push(JSON.parse(localStorage.getItem(`socio1`)));
+        socios.push(JSON.parse(localStorage.getItem(`socio2`)));
+
+            if (localStorage.getItem(`socio0`)){
+                for (let socio of socios) {
+                    DIV.innerHTML += `
                     <div class="tarjeta">
-                        <img src="${data.results[0].picture.large}">
-                        <p><span class="hand-font">Nombre:</span> ${data.results[0].name.first} ${data.results[0].name.last}</p>
-                        <p><span class="hand-font">Contacto:</span> ${data.results[0].email}</p>
-                        <p><span class="hand-font">País:</span> ${data.results[0].location.country}</p>
+                        <img src="${socio.results[0].picture.large}">
+                        <p><span class="hand-font">Nombre:</span> ${socio.results[0].name.first} ${socio.results[0].name.last}</p>
+                        <p><span class="hand-font">Contacto:</span> ${socio.results[0].email}</p>
+                        <p><span class="hand-font">País:</span> ${socio.results[0].location.country}</p>
                     </div>
-                `;
-
-            });
-        };
+                    `;
+                }
+                
+                BUTTON_FETCH.classList.add('ocultar');
+                return;
+            } else {
+                
+            for(let i = 0; i < 3; i++){
+                fetch('https://randomuser.me/api/')
+                .then(response => response.json())
+                .then(data => {
+                    localStorage.setItem(`socio${i}`, JSON.stringify(data))
+                    DIV.innerHTML += `
+                        <div class="tarjeta">
+                            <img src="${data.results[0].picture.large}">
+                            <p><span class="hand-font">Nombre:</span> ${data.results[0].name.first} ${data.results[0].name.last}</p>
+                            <p><span class="hand-font">Contacto:</span> ${data.results[0].email}</p>
+                            <p><span class="hand-font">País:</span> ${data.results[0].location.country}</p>
+                        </div>
+                    `;
+    
+                });
+            };
+            BUTTON_FETCH.classList.add('ocultar');
+        }
+            
     });
 
 });
